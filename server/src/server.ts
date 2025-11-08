843e449a8e856bdc828397800d3b0feee6e4e6ff
Krishan-Malinda <krishanmalinda99@gmail.com>
2025-11-08 17:47:51 +0530

    Revert "Use SocketIO to increase the responsing speed"
    This reverts commit a41961d93e383a0732874cf0df57051b38e0833b.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,10 @@
 import express from 'express';
 import cors from 'cors';
-import http from 'http'
 import 'dotenv/config';
-
 import chatRoutes from './routes/chatRoutes.ts'
+
 import { PORT } from "./constants/env.ts";
 import { FRONTEND_URL } from "./constants/env.ts";
-import socketHandler from './utils/socketHandler.ts';
-import attachIO from './middlewares/attachIO.ts';
 
 const app = express();
 
@@ -16,11 +13,7 @@ app.use(cors({
 }));
 app.use(express.json());
 
-const httpServer = http.createServer(app);
-const io = socketHandler(httpServer);
-
-
-app.use('/api/chat', attachIO(io), chatRoutes);
+app.use('/api/chat', chatRoutes);
 
 app.get("/", async (req, res, next) => {
     return res.status(200).send("Hello, Welcome to Ollama API Server.");
